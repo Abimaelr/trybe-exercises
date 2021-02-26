62e26d195c72a7b4ada5d23b7fb408641ace7dea
Abimael Rocha <abimaelra.lbu@gmail.com>
2021-02-26 10:17:14 -0300

    E1 ok

---
+++ js/script.js
@@ -0,0 +1,35 @@
+console.log('\n---------1--------\n\n');
+let n = 5;
+
+for(let linha = 0; linha < n; linha++){
+    let saida = "";
+    for(let coluna = 0; coluna < n; coluna++){
+        saida += "*";
+    }
+    console.log(saida);
+
+}
+
+
+
+console.log('\n---------2--------\n\n');
+
+
+
+
+console.log('\n---------3--------\n\n');
+
+
+
+
+console.log('\n---------4--------\n\n');
+
+
+
+
+console.log('\n---------b1--------\n\n');
+
+
+
+
+console.log('\n---------b2--------\n\n');
