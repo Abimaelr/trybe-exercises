cebe731036d8e7afba2f0a78ec4d501a33bbf2ac
Abimael <abimaelr.albu@gmail.com>
2021-03-05 18:34:41 -0300

    Holiday

--- js/script.js
+++ js/script.js
@@ -16,13 +16,19 @@ function createDaysOfTheWeek() {
   // Escreva seu código abaixo.
 
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const holidays = [24,25,31]
 
   //#days is a ID of the ul
-  //#day is a ID of the li
+  //.day is a class of the li
   const daysList = document.querySelector('#days')
   for(let index = 0; index < dezDaysList.length; index++){
       const dayElement = document.createElement('li')
       dayElement.innerText = dezDaysList[index]
+      dayElement.className = 'day'
+      if(holidays.includes(dezDaysList[index])){
+        dayElement.className += ' holiday'
+      }
+      
       daysList.appendChild(dayElement)
       
   }
