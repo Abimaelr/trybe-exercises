3cb7422e1a991e44832e780700cffa9999d18fcc
Abimael <abimaelr.albu@gmail.com>
2021-03-05 18:29:16 -0300

    Dias Inseridos

--- js/script.js
+++ js/script.js
@@ -14,3 +14,18 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+  //#days is a ID of the ul
+  //#day is a ID of the li
+  const daysList = document.querySelector('#days')
+  for(let index = 0; index < dezDaysList.length; index++){
+      const dayElement = document.createElement('li')
+      dayElement.innerText = dezDaysList[index]
+      daysList.appendChild(dayElement)
+      
+  }
+
+
+
