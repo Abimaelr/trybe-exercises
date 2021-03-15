ebeb6425afdd2ddf0a0397ac46c3c848e5e0d3f5
Abimael <abimaelr.albu@gmail.com>
2021-03-15 18:41:07 -0300

    Esqueleto montado

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const statesList = ['Selecione uma opção','----------------','Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
+
+const state = document.querySelector('#states');
+
+for(let index = 0; index < statesList.length; index += 1){
+    const stateItem = document.createElement('option')
+    
+    stateItem.innerText = statesList[index];
+
+    state.appendChild(stateItem);
+}
